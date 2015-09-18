0c4db7bf6068c35270525cf05cd4c2fda6e5d17d
lorenzop <lorenzo@poixson.com>
2015-09-18 16:01:21 -0400

    finished composer goal (untested)

--- src/xbuild.pl
+++ src/xbuild.pl
@@ -328,8 +328,51 @@ EOF
 
 
 sub goal_composer {
-print "Building with composer..\n";
 error ("Sorry, this goal is unfinished!");
+	# ensure tools are available
+	system 'which php      >/dev/null || { echo "PHP is not available - yum install php56w"; exit 1; }';
+	system 'which composer >/dev/null || { echo "Composer is not available - yum install php-tools"; exit 1; }';
+
+	my $pwd = getcwd;
+	my $path = "$pwd";
+	if ( ! -d "$path/" ) {
+		error ("Composer workspace not found: $path/");
+		exit 1;
+	}
+	if ( ! -f "$path/composer.json" ) {
+		error ("composer.json file not found: $path/");
+		exit 1;
+	}
+
+	# composer install
+	{
+		my $path = "$pwd";
+		print "Composer update: $path\n";
+		my $cmd = <<EOF;
+pushd "$path" && \\
+php `which composer` update -v --working-dir "$path/" || \\
+{ echo "Failed to run composer install command: $path/"; exit 1; }
+popd
+EOF
+		debug ("COMMAND:\n$cmd");
+		system $cmd;
+		# run phpunit if available
+		if ( -f "$path/vendor/bin/phpunit" ) {
+			my $cmd = <<EOF;
+pushd "$path" && \\
+php "$path/vendor/bin/phpunit" \\
+	--coverage-html="$path/coverage/html/" \\
+	--coverage-php="$path/coverage/coverage.php/" \\
+	--coverage-text="$path/coverage/coverage.txt" \\
+	--coverage-xml="$path/coverage/xml/" || \\
+{ echo "Failed to run composer install command: $path/"; exit 1; }
+popd
+EOF
+			debug ("COMMAND:\n$cmd");
+			system $cmd;
+		}
+	}
+
 }
 
 
