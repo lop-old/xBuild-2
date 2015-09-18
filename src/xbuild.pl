9915e3d325b3eadc2f4908b4d8d0c6e51d3428ce
lorenzop <lorenzo@poixson.com>
2015-09-18 16:00:32 -0400

    finished rpm goal

--- src/xbuild.pl
+++ src/xbuild.pl
@@ -27,6 +27,7 @@
 use strict;
 use warnings;
 use POSIX;
+use File::Copy;
 use JSON;
 use Switch;
 #use Cwd;
@@ -275,8 +276,53 @@ error ("Sorry, this goal is unfinished!");
 
 
 sub goal_rpm {
-print "Building rpm..\n";
-error ("Sorry, this goal is unfinished!");
+	# ensure tools are available
+	system 'which rpmbuild >/dev/null || { echo "Composer is not available - yum install rpm-build"; exit 1; }';
+	my $pwd = getcwd;
+	my $RPM_SPEC   = "$project_name.spec";
+	my $BUILD_ROOT = "$pwd/rpmbuild-root";
+	my $RPM_SOURCE = "$pwd";
+	my $ARCH       = "noarch";
+my $BUILD_NUMBER = 0;
+#my $SOURCE_PATH = "$BUILD_ROOT/";
+#my $SOURCE_FILE = "";
+	if ( ! -f "$pwd/$RPM_SPEC" ) {
+		error ("Spec file not found: $RPM_SPEC");
+		exit 1;
+	}
+	debug ("Found spec file: $RPM_SPEC");
+	# create build space
+	debug ("Creating directory: rpmbuild-root/");
+	mkdir "$BUILD_ROOT/" unless -d "$BUILD_ROOT/";
+	foreach my $dir ( 'BUILD', 'BUILDROOT', 'RPMS', 'SOURCE', 'SOURCES', 'SPECS', 'SRPMS', 'tmp' ) {
+		debug ("Creating directory: rpmbuild-root/$dir/");
+		mkdir "$BUILD_ROOT/$dir/" unless -d "$BUILD_ROOT/$dir/";
+	}
+	copy ( "$pwd/$RPM_SPEC", "$BUILD_ROOT/SPECS/" ) or error ("Failed to copy .spec file!");
+#	# copy source file
+#	if ($SOURCE_PATH ne "$BUILD_ROOT/SOURCES/") {
+#		copy ( "$SOURCE_PATH$SOURCE_FILE", "$BUILD_ROOT/SOURCES/" ) or error ("Failed to copy source file: $SOURCE_FILE");
+#	}
+#	if (length($SOURCE_PATH) == 0) {
+#		$SOURCE_PATH
+#	}
+
+	# build rpm
+	{
+		my $cmd = <<EOF;
+rpmbuild -bb \\
+	--target $ARCH \\
+	--define="_topdir $BUILD_ROOT" \\
+	--define="_tmppath $BUILD_ROOT/tmp" \\
+	--define="SOURCE_ROOT $RPM_SOURCE" \\
+	--define="_rpmdir $pwd/target" \\
+	--define="BUILD_NUMBER $BUILD_NUMBER" \\
+	"$BUILD_ROOT/SPECS/$RPM_SPEC" \\
+		|| exit 1
+EOF
+		debug ("COMMAND:\n$cmd");
+		system $cmd;
+	}
 }
 
 
