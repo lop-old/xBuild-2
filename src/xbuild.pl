251de3f0627542a19b4c6bf94da24151998cf8e5
lorenzop <lorenzo@poixson.com>
2015-09-18 14:05:21 -0400

    finished clean goal

--- src/xbuild.pl
+++ src/xbuild.pl
@@ -190,6 +190,14 @@ exit 0;
 
 sub goal_clean {
 	print "Cleaning..\n";
+	my $pwd = getcwd;
+	foreach my $dir ( 'target', 'rpmbuild-root', 'build', 'bin', 'out' ) {
+		my $path = "$pwd/$dir/";
+		if ( -d "$path" ) {
+			debug ("deleting path: $path");
+			system ("[ -z \"$path\" ] || rm -Rf --preserve-root \"$path\" || exit 1");
+		}
+	}
 }
 
 
