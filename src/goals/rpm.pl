fc99bcf726441fb46fab5501c6740aa79f4b783b
lorenzop <lorenzo@poixson.com>
2015-09-24 22:27:39 -0400

    cleaned up and finished rpm goal fixed proper handling of multiple arch's

--- src/goals/rpm.pl
+++ src/goals/rpm.pl
@@ -4,54 +4,114 @@ use warnings;
 
 
 sub goal_rpm {
-error ("Sorry, this goal is unfinished!");
+	print "Building rpm..\n";
+	my $goal_config = shift;
+	my $arch = 'noarch';
+	if (exists $goal_config->{Arch}) {
+		$arch = $goal_config->{Arch};
+	}
+	if (length($arch) == 0) {
+		error ("Arch argument not set in goal_rpm() call!");
+		exit 1;
+	}
 	# ensure tools are available
 	bin_file_exists ("rpmbuild");
+	# build multi-arch
+	if (index($arch, ',') != -1) {
+		ARCH_LOOP:
+		for $a ( split (/\,[\s]*/, $arch) ) {
+			if (length($a) == 0) {
+				next ARCH_LOOP;
+			}
+			goal_rpm ($goal_config, $a);
+		}
+		return;
+	}
+	goal_rpm_build ($goal_config, $arch);
+}
+
+
+
+sub goal_rpm_build {
+	my $goal_config = shift;
+	my $RPM_ARCH    = shift;
 	my $PWD = getcwd;
 	my $RPM_SPEC   = "${main::project_name}.spec";
-	my $BUILD_ROOT = "$PWD/rpmbuild-root";
+	my $BUILD_ROOT = "rpmbuild-root";
 	my $RPM_SOURCE = "$PWD";
-#	@{$config->{'Version Files'}};
-	my $RPM_ARCH   = "noarch";
-my $BUILD_NUMBER = 0;
-#my $SOURCE_PATH = "$BUILD_ROOT/";
-#my $SOURCE_FILE = "";
+	my $BUILD_NUMBER = $main::project_build_number;
+	if ($BUILD_NUMBER ne 'x' || length($BUILD_NUMBER) == 0) {
+		$BUILD_NUMBER = int ($main::project_build_number);
+	}
+	my $SOURCE_PATH = "";
+	my $SOURCE_FILE = "";
+	if (exists $goal_config->{Source}) {
+		$SOURCE_PATH = $goal_config->{Source};
+	}
+	# .spec file exists
 	if ( ! -f "$PWD/$RPM_SPEC" ) {
 		error ("Spec file not found: $RPM_SPEC");
 		exit 1;
 	}
 	debug ("Found spec file: $RPM_SPEC");
 	# create build space
-	debug ("Creating directory: rpmbuild-root/");
-	mkdir "$BUILD_ROOT/" unless -d "$BUILD_ROOT/";
-	foreach my $dir ( 'BUILD', 'BUILDROOT', 'RPMS', 'SOURCE', 'SOURCES', 'SPECS', 'SRPMS', 'tmp' ) {
-		debug ("Creating directory: rpmbuild-root/$dir/");
-		mkdir "$BUILD_ROOT/$dir/" unless -d "$BUILD_ROOT/$dir/";
+	if ( ! -d "$PWD/$BUILD_ROOT/" ) {
+		debug ("Creating directory: $BUILD_ROOT/");
+		mkdir "$PWD/$BUILD_ROOT/";
 	}
-	copy ( "$PWD/$RPM_SPEC", "$BUILD_ROOT/SPECS/" ) or error ("Failed to copy .spec file!");
-#	# copy source file
-#	if ($SOURCE_PATH ne "$BUILD_ROOT/SOURCES/") {
-#		copy ( "$SOURCE_PATH$SOURCE_FILE", "$BUILD_ROOT/SOURCES/" ) or error ("Failed to copy source file: $SOURCE_FILE");
-#	}
-#	if (length($SOURCE_PATH) == 0) {
-#		$SOURCE_PATH
-#	}
-
+	foreach my $dir ( 'BUILD', 'BUILDROOT', 'RPMS', 'SOURCE', 'SOURCES', 'SPECS', 'SRPMS', 'tmp' ) {
+		if ( ! -d "$PWD/$BUILD_ROOT/$dir/" ) {
+			debug ("Creating directory: $BUILD_ROOT/$dir/");
+			mkdir "$PWD/$BUILD_ROOT/$dir/";
+		}
+	}
+	# copy .spec file to rpmbuild-root/SPECS/
+	copy ( "$PWD/$RPM_SPEC", "$PWD/$BUILD_ROOT/SPECS/" ) or error ("Failed to copy .spec file!");
+	# get source file
+	if (length($SOURCE_PATH) > 0) {
+		($SOURCE_FILE = $SOURCE_PATH) =~ s/.*\///;
+		if (length($SOURCE_FILE) == 0) {
+			error ("File name must be specified in source path: $SOURCE_PATH");
+			exit 1;
+		}
+		# download source file
+		if ( $SOURCE_PATH =~ m/^(http|https):\/\/.*/ ) {
+			debug ("Downloading source file: $SOURCE_FILE  from: $SOURCE_PATH");
+			my $cmd = "wget -O \"$PWD/$BUILD_ROOT/SOURCES/$SOURCE_FILE\" \"$SOURCE_PATH\" ".
+				"|| { echo \"Failed to download source file!\"; exit 1; }";
+			debug ("Running command: $cmd");
+			print "\n";
+			system ($cmd) and error ("Failed!!!");
+			print "\n";
+		# copy local source file
+		} else {
+			if ( ! -f "$PWD/$SOURCE_PATH" ) {
+				error ("Source file not found: $SOURCE_PATH");
+				exit 1;
+			}
+			debug ("Making copy of source file: $SOURCE_PATH  to: $BUILD_ROOT/SOURCES/");
+			copy ("$PWD/$SOURCE_PATH", "$PWD/$BUILD_ROOT/SOURCES/") or error ("Failed to copy source file: $SOURCE_PATH");
+		}
+	}
 	# build rpm
 	{
 		my $cmd = <<EOF;
 rpmbuild -bb \\
 	--target="$RPM_ARCH" \\
-	--define="_topdir $BUILD_ROOT" \\
-	--define="_tmppath $BUILD_ROOT/tmp" \\
-	--define="SOURCE_ROOT $RPM_SOURCE" \\
-	--define="_rpmdir $PWD/target" \\
+	--define="_topdir $PWD/$BUILD_ROOT/" \\
+	--define="_tmppath $PWD/$BUILD_ROOT/tmp/" \\
+	--define="SOURCE_ROOT $RPM_SOURCE/" \\
+	--define="_rpmdir $PWD/target/" \\
 	--define="BUILD_NUMBER $BUILD_NUMBER" \\
-	"$BUILD_ROOT/SPECS/$RPM_SPEC" \\
+	"$PWD/$BUILD_ROOT/SPECS/$RPM_SPEC" \\
 		|| exit 1
 EOF
+		# run rpmbuild command
 		debug ("COMMAND:\n$cmd");
+		print "\n\n";
 		system ($cmd) and error ("Failed to build rpm!");
+		print "\n\nResults: ";
+		system ("ls -l $PWD/target/");
 	}
 }
 
