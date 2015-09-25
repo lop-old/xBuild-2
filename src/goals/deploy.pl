34b5f75b2f9cefcbc35e8b52539622fd15e96bed
lorenzop <lorenzo@poixson.com>
2015-09-25 18:17:36 -0400

    renamed <PROJECT_NAME> <PROJECT_VERSION> <PROJECT_BUILD_NUMBER> tags added replace_tags() function to handle this

--- src/goals/deploy.pl
+++ src/goals/deploy.pl
@@ -12,4 +12,17 @@ error ("Sorry, this goal is unfinished!");
 
 
 
+sub goals_deploy_replace_tags {
+	my $file = shift;
+	if (! defined $file || length($file) == 0) {
+		return "";
+	}
+	$file =~ s/\<PROJECT_NAME\>/${main::project_name}/g;
+	$file =~ s/\<PROJECT_VERSION\>/${main::project_version}/g;
+	$file =~ s/\<PROJECT_BUILD_NUMBER\>/${main::project_build_number}/g;
+	return $file;
+}
+
+
+
 1;
