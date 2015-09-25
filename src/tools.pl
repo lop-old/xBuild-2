134954459fd972795a667f9b54dc1ee5be6ef932
lorenzop <lorenzo@poixson.com>
2015-09-25 15:19:55 -0400

    moved logging functions from xbuild.pl to tools.pl

--- src/tools.pl
+++ src/tools.pl
@@ -45,4 +45,116 @@ sub split_comma {
 
 
 
+##################################################
+### logging
+
+
+
+sub title {
+	big_title ( shift );
+}
+sub small_title {
+	my $title = shift;
+	my @lines = split /\n/, $title;
+	my $maxlen = 0;
+	LINES_LOOP:
+	foreach my $line (@lines) {
+		my $len = length($line);
+		if ($len == 0) {
+			next LINES_LOOP;
+		}
+		if ($len > $maxlen) {
+			$maxlen = $len;
+		}
+	}
+	if ($maxlen == 0) {
+		return;
+	}
+	my $full  = ( '*' x ($maxlen + 8) );
+	my $blank = ( ' ' x $maxlen );
+	print "\n\n";
+	print " $full \n";
+	foreach my $line (@lines) {
+		my $padding = $maxlen - length($line);
+		my $padfront = ( ' ' x floor ($padding / 2) );
+		my $padend   = ( ' ' x ceil  ($padding / 2) );
+		print " **  $padfront$line$padend  ** \n";
+	}
+	print " $full \n";
+	print "\n";
+}
+sub big_title {
+	my $title = shift;
+	my @lines = split /\n/, $title;
+	my $maxlen = 0;
+	LINES_LOOP:
+	foreach my $line (@lines) {
+		my $len = length($line);
+		if ($len == 0) {
+			next LINES_LOOP;
+		}
+		if ($len > $maxlen) {
+			$maxlen = $len;
+		}
+	}
+	if ($maxlen == 0) {
+		return;
+	}
+	my $full  = ( '*' x ($maxlen + 10) );
+	my $blank = ( ' ' x $maxlen );
+	print "\n\n";
+	print " $full \n";
+	print " $full \n";
+	print " ***  $blank  *** \n";
+	foreach my $line (@lines) {
+		my $padding = $maxlen - length($line);
+		my $padfront = ( ' ' x floor ($padding / 2) );
+		my $padend   = ( ' ' x ceil  ($padding / 2) );
+		print " ***  $padfront$line$padend  *** \n";
+	}
+	print " ***  $blank  *** \n";
+	print " $full \n";
+	print " $full \n";
+	print "\n";
+}
+
+
+
+sub debug {
+	if ($main::debug == 0) {
+		return;
+	}
+	my $msg = shift;
+	if (! defined $msg || length($msg) == 0) {
+		print "\n";
+		return;
+	}
+	my @lines = split /\n/, $msg;
+	LINES_LOOP:
+	foreach my $line (@lines) {
+		if (length($line) == 0) {
+			next LINES_LOOP;
+		}
+		print " [debug]  $line\n";
+	}
+}
+sub error {
+	my $msg = shift;
+	my $err = shift;
+	if (!defined $msg || length($msg) == 0) {
+		$msg = "Failed unexpectedly!";
+	}
+	if (!defined $err || length($err) == 0 || $err eq "0") {
+		$err = 1;
+	}
+	print "\n\n";
+	print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
+	print "\n [ERROR:$err]  $msg\n\n";
+	print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
+	print "\n\n";
+	exit 1;
+}
+
+
+
 1;
